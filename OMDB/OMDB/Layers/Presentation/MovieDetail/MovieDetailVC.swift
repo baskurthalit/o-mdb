c7ca9714423db6f43fdbd4a1508a2aae124a66dc
Halit Seyfullah Baskurt <hbaskurt@invio.com>
2023-07-24 20:31:57 +0300

    View model observation, movie detail'e eklendi.

--- OMDB/OMDB/Layers/Presentation/MovieDetail/MovieDetailVC.swift
+++ OMDB/OMDB/Layers/Presentation/MovieDetail/MovieDetailVC.swift
@@ -13,9 +13,27 @@ class MovieDetailVC: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addViewModelObservation()
+        
     }
+    
+    private func addViewModelObservation() {
+        viewModel.stateClosure = { [weak self] viewModelState in
+            switch viewModelState{
+            case .updateUI(let data): self?.viewModelObservationHandler(data)
+            case .error(let error): break
+            }
+        }
+    }
+    
+    private func viewModelObservationHandler(_ state: MovieDetailVMImpl.Event?) {
+        switch state {
+        case .updateUI: break
+        default: break
+        }
+    }
+    
+
     
     func inject(viewModel: MovieDetailVM) {
         self.viewModel = viewModel
