5630125368c637a59b2151a225b68b43bc8061df
Halit Seyfullah Baskurt <hbaskurt@invio.com>
2023-07-25 19:28:38 +0300

    Connection alert, scene delegate'e taşındı.

--- OMDB/OMDB/AppDelegate.swift
+++ OMDB/OMDB/AppDelegate.swift
@@ -15,9 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private var reachability: ReachabilityManager = .shared
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(reachabilityChanged(_:)),
-                                               name: Notification.Name("internetConnectionChanged"), object: nil)
         reachability.startMonitoring()
         FirebaseApp.configure()
         
@@ -38,26 +35,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    @objc func reachabilityChanged(_ notification: NSNotification) {
-        if let object = notification.object as? [String : Any],
-           let isNetworkAvailable : Bool = object["isNetworkAvailable"] as? Bool {
-            
-            if isNetworkAvailable{
-                if connectionAlert != nil {
-                    connectionAlert?.dismiss(animated: true) { self.connectionAlert = nil }
-                }
-            } else {
-                guard let topVC = UIApplication.getTopViewController(), topVC != connectionAlert else { return }
-                connectionAlert = UIAlertController(title: "İnternet Bağlantısı YOK", message: "SERVICE_ERROR", preferredStyle: .alert)
-                connectionAlert?.addAction(.init(title: "OK", style: .cancel, handler: { alertAction in
-                    self.connectionAlert = nil
-                }))
-                
-                if let connectionAlert, topVC != connectionAlert { topVC.present(connectionAlert, animated: true) }
-            }
-        }
-    }
-    
 }
 
--- OMDB/OMDB/SceneDelegate.swift
+++ OMDB/OMDB/SceneDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     private(set) var appCoordinator: AppCoordinator?
+    private var connectionAlert : UIAlertController?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(reachabilityChanged(_:)),
+                                               name: Notification.Name("internetConnectionChanged"), object: nil)
         
         let baseNavigationController: BaseNavigationController = .init()
         
@@ -25,34 +30,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.makeKeyAndVisible()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    @objc func reachabilityChanged(_ notification: NSNotification) {
+        if let object = notification.object as? [String : Any],
+           let isNetworkAvailable : Bool = object["isNetworkAvailable"] as? Bool {
+            
+            if isNetworkAvailable{
+                if connectionAlert != nil {
+                    connectionAlert?.dismiss(animated: true) { self.connectionAlert = nil }
+                }
+            } else {
+                guard let topVC = UIApplication.getTopViewController(base: appCoordinator?.navigationController),
+                      topVC != connectionAlert else { return }
+                connectionAlert = UIAlertController(title: "İnternet Bağlantısı YOK", message: "SERVICE_ERROR", preferredStyle: .alert)
+                connectionAlert?.addAction(.init(title: "OK", style: .cancel, handler: { alertAction in
+                    self.connectionAlert = nil
+                }))
+                
+                if let connectionAlert, topVC != connectionAlert { topVC.present(connectionAlert, animated: true) }
+            }
+        }
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
 
 }
 
